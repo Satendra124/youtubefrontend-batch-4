8bd21b90689c49daab2058f0e76772eae8eb375b
Satendra Raj <satendra.raj@nium.com>
2023-11-06 13:51:08 +0530

    buitify

--- src/components/VideoComponent.js
+++ src/components/VideoComponent.js
@@ -1,17 +1,23 @@
+import { Link } from 'react-router-dom';
 const VideoComponent = () => {
   return (
     <div className="video">
       <div className="thumbnail">
-        <img
-          src={
-            "https://i.ytimg.com/vi/Hy55Ij78amM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCqIcu-Eej1ZcceapvJYaKVwSbCFQ"
-          }
-          className="thumbnailImage"
-        />
+        <Link to={'/video'}>
+          <img
+            alt="thumbnail"
+            src={
+              "https://i.ytimg.com/vi/`Hy55Ij78amM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCqIcu-Eej1ZcceapvJYaKVwSbCFQ"
+            }
+            className="thumbnailImage"
+          />
+        </Link>
+
       </div>
       <div className="titlewrapper">
         <div className="channelphoto">
           <img
+            alt="channelImage"
             src={
               "https://yt3.ggpht.com/dxED1O-r5cRS73JBlUk4VS3pZHDfiHcuRjRbFMcf6KgYhxP4NUlD7x0h4TR1XTXkl-JGjTPTYQ=s68-c-k-c0x00ffffff-no-rj"
             }
@@ -20,10 +26,11 @@ const VideoComponent = () => {
         </div>
         <div className="title">
           This is a video Title for my video in youtube
+          <div className="channel">MrWhosTheBoss</div>
+          <div className="views">1.7M Views . 18 Hours Ago</div>
         </div>
       </div>
-      <div className="channel">MrWhosTheBoss</div>
-      <div className="views">1.7M Views . 18 Hours Ago</div>
+
     </div>
   );
 };
--- src/components/VideoPage.js
+++ src/components/VideoPage.js
@@ -1,10 +1,31 @@
 import HeaderComponent from "./HeaderComponent";
 import "./VideoPage.css";
+
+
+function SuggestedVideo(){
+  return (<>
+    <div className='suggested-video'>
+      <div className='suggestion-image'>
+        <img src="https://i.ytimg.com/vi/SlPhMPnQ58k/mqdefault.jpg" width={'230px'} />
+      </div>
+      <div className='suggestion-content'>
+        <div class="video-info">
+            <p class="video-sug-title">
+              Kalank Title Track - Lyrical | Alia Bhatt
+            </p>
+            <p class="margin-0 smaller-fontsize">T-Series</p>
+            <p class="margin-0 smaller-fontsize">230M views . 4 years ago</p>
+        </div>
+      </div>
+    </div>
+  </>);
+}
+
 const VideoPage = () => {
   return (
-    <div className="wrapper">
+    <div className="wrapper-video">
       <HeaderComponent />
-      <div className="main">
+      <div className="main-video">
         <div className="videoWrapper">
           <iframe
             src="https://www.youtube.com/embed/Hy55Ij78amM?si=U3aB-mSFPb4Nhhc_"
@@ -14,9 +35,9 @@ const VideoPage = () => {
             allowfullscreen
             className="iframe"
           ></iframe>
-          <div className="title"> This is the title fo the page</div>
+          <div className="title-video"> This is the title fo the page</div>
           <div className="channelWrapper">
-            <div className="channel">
+            <div className="channel-video">
               <img
                 src={
                   "https://yt3.ggpht.com/dxED1O-r5cRS73JBlUk4VS3pZHDfiHcuRjRbFMcf6KgYhxP4NUlD7x0h4TR1XTXkl-JGjTPTYQ=s68-c-k-c0x00ffffff-no-rj"
@@ -27,7 +48,15 @@ const VideoPage = () => {
             <div className="likesWrappper"></div>
           </div>
         </div>
-        <div className="suggestions"></div>
+        <div className="suggestions">
+          <SuggestedVideo />
+          <SuggestedVideo />
+          <SuggestedVideo />
+          <SuggestedVideo />
+          <SuggestedVideo />
+          <SuggestedVideo />
+          
+        </div>
       </div>
     </div>
   );
